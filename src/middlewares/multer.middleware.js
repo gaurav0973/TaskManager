b7e34e65cfe1ef720c0b5e15706d6a49927d1d53
gaurav0973 <codejourneypanda@gmail.com>
2025-04-15 23:16:09 +0530

    File uploading

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,17 +1,17 @@
 import multer from "multer"
 
+// store in the diskstorage => local server
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // This storage needs public/images folder in the root directory
-    // Else it will throw an error saying cannot find path public/images
-    cb(null, `./public/images`);
+    cb(null,"./public/images");
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
-// Middleware responsible to read form data and upload the File object to the mentioned path
+
+
 export const upload = multer({
   storage,
   limits: {
